34b807fc48fa61ad1a886ee5829f5750ca5f3f8b
liad_raz <raz.liad@gmail.com>
2021-05-12 20:07:32 -0400

    Rough HTML and CSS layout was made. Basic math operator functions added. HTML and JS file are not fully wired.

---
+++ script.js
@@ -0,0 +1,56 @@
+// Basic operators 
+function add(a, b) {
+    return (a + b);
+}
+
+function subtrat(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+
+function operate(num1, num2) {
+    switch (true) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    } 
+}
+
+// Create Buttons Grid
+function createDigitsGrid() {
+    buttonsGrid.classList.add('grid');
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 4; j++) {
+            createButton();
+        }
+    }
+}
+
+let buttonsGrid = document.querySelector('.buttons-grid'); 
+
+// Create button
+function createButton() {
+    let button = document.createElement('button');
+    button.classList.add('button');
+    buttonsGrid.appendChild(button);
+}
+
+createDigitsGrid()
+
